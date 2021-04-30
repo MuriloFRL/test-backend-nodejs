67f10845f44346576ba7972d81557eb316504a58
Murilo <murilo_arvoredo@hotmail.com>
2021-04-30 18:14:46 -0300

    add final routes and filtered search

--- routes/products.js
+++ routes/products.js
@@ -12,9 +12,49 @@ router.get('/products', async (req, res)=>{
     }
 })
 
+//route to user get a list of products filtered by title
+router.get('/products/filter/title', async (req, res)=>{
+    try{
+        const products = await Product.find({title : req.body.title})
+        res.json(products)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//route to user get a list of products filtered by description
+router.get('/products/filter/description', async (req, res)=>{
+    try{
+        const products = await Product.find({description : req.body.description})
+        res.json(products)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//route to user get a list of products filtered by price
+router.get('/products/filter/price', async (req, res)=>{
+    try{
+        const products = await Product.find({price : req.body.price})
+        res.json(products)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//route to user get a list of products filtered by category
+router.get('/products/filter/category', async (req, res)=>{
+    try{
+        const products = await Product.find({category : req.body.category})
+        res.json(products)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
 //route to user get a list of products filtered by ID
 router.get('/products/:id', getProduct, (req, res)=>{
-
+    res.json(res.product)
 })
 
 //route to user add a product to the base
@@ -35,16 +75,31 @@ router.post('/products', async(req, res)=>{
 })
 
 //route to user update a product category
-router.patch('/products/:id', getProduct, (req, res)=>{
-
+router.patch('/products/:id', getProduct, async (req, res)=>{
+    if(req.body.title != null) res.product.title = req.body.title
+    if(req.body.description != null) res.product.description = req.body.description
+    if(req.body.price != null) res.product.price = req.body.price
+    if(req.body.category != null) res.product.category = req.body.category
+    
+    try{
+        const updatePorduct = await res.product.save()
+        res.json(updatePorduct)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //route to user delete a product from the base
-router.delete('/products/:id', getProduct, (req, res, next)=>{
-
+router.delete('/products/:id', getProduct, async(req, res)=>{
+    try{
+        await res.product.remove()
+        res.json({ message: 'product deleted'})
+    }catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-async function getProduct(req, res){
+async function getProduct(req, res, next){
     try{
         product = await Product.findById(req.params.id)
         if (product == null) return res.status(404).json({ message: 'product not found'})
