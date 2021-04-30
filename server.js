8121e7de3fbf27de116df441c57065b985f973ba
Murilo <murilo_arvoredo@hotmail.com>
2021-04-30 15:17:21 -0300

    add routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost/products", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (err) => console.error('error', err))
 db.once('open', () => console.log('DB connection established'))
+
+app.use(express.json())
 
 app.listen(3000, () => console.log('server running at 3000'))
